3651f3a57767471987c39eca93c2bbd166d737d6
Hesam Taherzadegan <Taherzadehesam@gmail.com>
2024-03-20 14:11:33 +0330

    added .gitignore

--- basics/build/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/hesam/Desktop/CUDA_Topics/basics
-# Build directory: /home/hesam/Desktop/CUDA_Topics/basics/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
